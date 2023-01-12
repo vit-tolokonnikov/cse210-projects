7f6bd043e0e571327cafffb00f7f2e512b3c76b4
vit-tolokonnikov <vit.tolokonnikov@gmail.com>
2023-01-13 05:24:47 +0700

    Prep05 was comleted

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -4,6 +4,41 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep5 World!");
+        DisplayWelcome();
+        string userName = PromptUserName();
+        int square = PromptUserNumber();
+
+        DisplayResult(userName, SquareNumber(square));
+    }
+
+    static void DisplayWelcome()
+    {
+        Console.WriteLine("Welcome to the Program!");
+    }
+    
+    static string PromptUserName()
+    {
+        Console.Write("Please enter your name: ");
+        string name = Console.ReadLine();
+
+        return name;
+    }
+
+    static int PromptUserNumber()
+    {
+        Console.Write("Please enter your favorite number: ");
+        int favoriteNumber = int.Parse(Console.ReadLine());
+
+        return favoriteNumber;
+    }
+
+    static int SquareNumber(int number)
+    {
+        return number * number;
+    }
+
+    static void DisplayResult(string userName, int square)
+    {
+        Console.WriteLine($"{userName}, the square of your number is {square}");
     }
 }
