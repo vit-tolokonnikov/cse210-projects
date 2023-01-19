51b08f3a1a60183fb6c6ed80357a535791397573
vit-tolokonnikov <vit.tolokonnikov@gmail.com>
2023-01-19 20:27:50 +0700

    Abstraction

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -5,5 +5,30 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Learning02 World!");
+
+        Job job1 = new Job();
+        
+        job1._jobTitle = "Software Engineer";
+        job1._company = "Microsoft";
+        job1._startYear = "2019";
+        job1._endYear = "2022";
+
+        job1.Display();
+
+        Job job2 = new Job();
+        
+        job2._jobTitle = "Manager";
+        job2._company = "Apple";
+        job2._startYear = "2022";
+        job2._endYear = "2023";
+
+        job2.Display();
+
+        Resume resume1 = new Resume();
+        resume1._name = "Vitaliy";
+        resume1._jobs.Add(job1);
+
+        resume1.Display();
+
     }
 }
