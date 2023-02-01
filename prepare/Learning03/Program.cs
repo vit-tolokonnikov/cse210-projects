fa67f1322e6ac5b113516934dd771922590063e4
vit-tolokonnikov <vit.tolokonnikov@gmail.com>
2023-02-02 03:59:00 +0700

    Encapsulation Learning Activity

--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -4,6 +4,20 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning03 World!");
+        Fraction test1 = new Fraction();
+        Console.WriteLine(test1.GetDecimalValue());
+        Console.WriteLine(test1.GetFractionString());
+
+        Fraction test2 = new Fraction(5);
+        Console.WriteLine(test2.GetDecimalValue());
+        Console.WriteLine(test2.GetFractionString());
+
+        Fraction test3 = new Fraction(3, 4);
+        Console.WriteLine(test3.GetDecimalValue());
+        Console.WriteLine(test3.GetFractionString());
+
+        Fraction test4 = new Fraction(1, 3);
+        Console.WriteLine(test4.GetDecimalValue());
+        Console.WriteLine(test4.GetFractionString());
     }
 }
