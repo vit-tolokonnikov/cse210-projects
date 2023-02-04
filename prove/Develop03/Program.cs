4ba314578309228e0a9f3a22a54219714f9fd71b
vit-tolokonnikov <vit.tolokonnikov@gmail.com>
2023-02-05 03:54:01 +0700

    Encapsulation - Design Activity

--- prove/Develop03/Program.cs
+++ prove/Develop03/Program.cs
@@ -5,5 +5,33 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Develop03 World!");
+
+        /*
+    
+        Scripture  (GetRenderedText, Display, HideWords) - to hold a whole scripture and to show rendered versions of it whenever it is called to do that.
+        Reference  (Reference, GetScriptureText) - To hold reference's information and use it to load and return a text scripture.
+        Word (Hide, Show) - to hold a single word from a scripture        
+
+        Attributes of Scripture:
+            _text: string
+            _reference: Reference
+            _listWord: List
+            _reference: Reference
+            _isCompletelyHidden: bool
+
+        Attributes of Reference:
+            _book: string
+            _chapter: int
+            _verse_start: int
+            _verse_end: int
+
+        Attributes of Word:
+            _isHidden: bool
+            _isCompletelyHidden: bool
+            _text: string
+            _renderedText: string
+
+
+        */
     }
 }
