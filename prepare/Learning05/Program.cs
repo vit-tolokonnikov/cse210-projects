22ec59a691d0ca551ba7bb6fffe184d43b25925a
vit-tolokonnikov <vit.tolokonnikov@gmail.com>
2023-03-02 09:04:25 +0700

    W09 Prepare: Learning Activity

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -4,6 +4,24 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+        List<Shape> shapes = new List<Shape>();
+
+        Square mySquare = new Square("Blue", 4);
+        shapes.Add(mySquare);
+
+        Rectangle myRectangle = new Rectangle("Green", 3, 8);
+        shapes.Add(myRectangle);
+
+        Circle myCircle = new Circle("Yellow", 7);
+        shapes.Add(myCircle);
+
+        foreach (Shape s in shapes)
+        {
+            string color = s.GetColor();
+
+            double area = s.GetArea();
+
+            Console.WriteLine($"The {color} shape has an area of {area}.");
+        }
     }
 }
